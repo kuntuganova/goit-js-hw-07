c395ee158ebfdb87be8fe5b2fcadca7e16e87744
kuntuganova <elya.kuntuganova@gmail.com>
2023-02-21 14:24:30 +0600

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,36 +4,33 @@ import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
-const items = [];
+const imagesMarkup = createImagesMarkup(galleryItems);
+gallery.insertAdjacentHTML("beforeend", imagesMarkup);
 
-galleryItems.forEach((el) => {
-  const galleryItem = document.createElement("div");
-  galleryItem.className = "gallery__item";
-  const galleryLink = document.createElement("a");
-  galleryLink.className = "gallery__link";
-  const galleryImage = document.createElement("img");
-  galleryImage.className = "gallery__img";
-  galleryImage.href = el.preview;
-  galleryItem.setAttribute("data-source", el.original);
-  galleryImage.alt = el.description;
-
-  galleryItem.append(galleryLink);
-  galleryItem.append(galleryImage);
-  items.push(galleryItem);
-});
-
-gallery.append(...items);
+function createImagesMarkup(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original.value}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+    })
+    .join("");
+}
 
 gallery.addEventListener("click", (e) => {
   e.preventDefault();
-  if (e.target.nodeName !== "IMG") {
-    return;
-  }
+  if (e.target.classList.contains("gallery")) return;
+  const source = e.target.dataset.source;
 
-  const selectedImage = e.target.getAttribute("data-source");
-  const instance = basicLightbox.create(
-    `<img src="${selectedImage}" width="800" height="600">`
-  );
+  const instance = basicLightbox.create(`
+    <img src="${source}"width="800" height="600">`);
 
   instance.show();
 
